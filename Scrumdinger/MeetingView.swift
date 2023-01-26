9c5e68e65c6cb4bf78a4416c63fbdbdeed22259c
Jessica-Mansell <jcmansell88@gmail.com>
2023-01-25 20:13:15 -0600

    added VStack line

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -11,7 +11,12 @@ struct MeetingView: View {
     var body: some View {
         ProgressView(value: 5, total: 15)
         HStack {
-            Text("Seconds Elapsed")
+            VStack {
+                Text("Seconds Elapsed")
+            }
+            VStack {
+                Text("Seconds Remaining")
+            }
         }
     }
 }
