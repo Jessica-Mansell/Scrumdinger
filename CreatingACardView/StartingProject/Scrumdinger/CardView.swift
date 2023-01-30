d27783f38d1f345875a3d48a5e13353067e8d945
Jessica-Mansell <jcmansell88@gmail.com>
2023-01-30 17:49:08 -0600

    change headline with scrum inputs

--- CreatingACardView/StartingProject/Scrumdinger/CardView.swift
+++ CreatingACardView/StartingProject/Scrumdinger/CardView.swift
@@ -3,7 +3,8 @@ import SwiftUI
 struct CardView: View {
     let scrum: DailyScrum
     var body: some View {
-        Text("Hello, World!")
+        Text(scrum.title)
+            .font(.headline)
     }
 }
 
@@ -11,5 +12,7 @@ struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.sampleData[0]
     static var previews: some View {
         CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
     }
 }
