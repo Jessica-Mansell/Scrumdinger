abc5bb6595db42d4cda771ba10ff9a4df16d4beb
Jessica-Mansell <jcmansell88@gmail.com>
2023-01-29 22:29:46 -0600

    add card view file

---
+++ CreatingACardView/StartingProject/Scrumdinger/CardView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
